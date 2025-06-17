f11d832c903c35906bf7bd34d12787cbe2ed187b
Nikitha26reddy <245122749061@mvsrec.edu.in>
2025-06-17 15:07:09 +0530

    Add files via upload

---
+++ SumAvg.java
@@ -0,0 +1,13 @@
+public class SumAvg{
+public static void main(String args[])
+{
+int sum=0;
+for(int i=0;i<=10;i++)
+sum+=i;
+double avg=sum/10.0;
+System.out.println("sum:"+sum);
+System.out.println("avg:"+avg);
+}
+}
+
+
